61cc1a7ed60aeab3459e537290ba0468c8739fee
youssef-md <emaildeyoussefmuhamad@gmail.com>
2021-03-18 20:34:50 -0300

    refactor: fga-eps-mds/2020.2-Lend.it#96 update sequelize config based on the NODE_ENV
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>

--- src/db/index.js
+++ src/db/index.js
@@ -11,7 +11,7 @@ class Database {
 
   init() {
     try {
-      this.connection = new Sequelize(databaseConfig.development);
+      this.connection = new Sequelize(databaseConfig[process.env.NODE_ENV]);
       models.forEach(model => model.init(this.connection));
     } catch (error) {
       console.log(error.message);
